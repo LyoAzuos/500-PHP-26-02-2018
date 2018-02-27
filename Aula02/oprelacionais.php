607cb0b9a6057f68f21e65a5e100d7e379694a66
Emílio dos Santos <emiliosansou@gmail.com>
2018-02-27 20:29:31 -0300

    operadores relacionais

---
+++ Aula02/oprelacionais.php
@@ -0,0 +1,57 @@
+<?php
+//Operadores Relacionais
+
+echo "<pre>";
+
+//igual
+var_dump(2 == 2);
+var_dump(2 == '2');
+var_dump(2 == 3);
+echo '<hr>';
+
+// Idêntico
+var_dump(2 === 2);
+var_dump(2 === '2');
+echo '<hr>';
+
+//Diferente
+var_dump(2 != 2);
+var_dump(2 != '2');
+var_dump(2 != 3);
+echo '<hr>';
+
+//Maior que
+var_dump(2 < 2);
+var_dump(2 < '2');
+var_dump(2 < 3);
+echo '<hr>';
+
+// Menor
+
+var_dump(2 > 2);
+var_dump(2 > '2');
+var_dump(2 > 3);
+echo '<hr>';
+
+// Maior igual
+
+var_dump(2 <= 2);
+var_dump(2 <= '2');
+var_dump(2 <= 3);
+echo '<hr>';
+
+// Menor Igual
+
+var_dump(2 >= 2);
+var_dump(2 >= '2');
+var_dump(2 >= 3);
+echo '<hr>';
+
+
+
+
+
+
+
+
+?>
