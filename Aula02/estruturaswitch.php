4f45992d4a5f0d6e05f0a1ebf0b72acf4bcea117
Emílio dos Santos <emiliosansou@gmail.com>
2018-02-27 22:22:17 -0300

    exercicio final

---
+++ Aula02/estruturaswitch.php
@@ -0,0 +1,17 @@
+<?php
+// Pagina 27 da apostila
+
+$nome = readline ('Digite seu nome:');
+
+switch ($nome) {
+	case 'Emílio':
+	echo 'Olá Emílio';
+		break;
+	
+	default:
+		# code...
+		break;
+}
+
+
+?>
