d560120fff5d2e8ca374b4ffeefc2d285149a209
Emílio dos Santos <emiliosansou@gmail.com>
2018-02-28 21:48:29 -0300

    operadores resolvidos

--- Aula03/Atividades/atvoperadores.php
+++ Aula03/Atividades/atvoperadores.php
@@ -1,4 +1,6 @@
 <?php
+
+echo "<pre>";
 /*
 Operadores Aritiméticos
 
@@ -10,6 +12,34 @@ Operadores Aritiméticos
     Crie uma variável com o valor da módulo(resto da divisão) das variáveis anteriores;
     Exiba os valores das variáveis;
 
+*/
+
+$a = 1;
+echo "A vale ". $a;
+echo "<hr>";
+$b = 4;
+echo "B vale ". $b;
+echo "<hr>";
+
+$soma = $a + $b;
+echo "RESULTADO - SOMA = ". $soma;
+echo "<hr>";
+$sub = $a - $b;
+echo "RESULTADO - SUBTRAÇÃO = " . $sub;
+echo "<hr>";
+$div = $a / $b;
+echo "RESULTADO - DIVISÃO = " . $div;
+echo "<hr>";
+$mult = $a * $b;
+echo "RESULTADO - MULTIPLICAÇÃO = " . $mult;
+echo "<hr>";
+$mod = $a % $b;
+echo "RESULTADO - MÓDULO = " . $mult;
+echo "<hr>";
+
+
+
+/*
 Operadores de atribuição, incremento e decremento
 
     Crie uma variável num com uma valor numérico(Pode ser qualquer valor);
@@ -17,9 +47,71 @@ Operadores de atribuição, incremento e decremento
     Use um operador de atruibuição e subtraia o valor 3 a variavel, exiba o valor atual da variavel;
     Use um operador de atruibuição e multiplique o valor 10 a variavel, exiba o valor atual da variavel;
     Use um operador de atruibuição e divida o valor 2 a variavel, exiba o valor atual da variavel;
-    Crie uma variavel cont = 0, utilize o operador de incremento até o valor da variavel ser 10 sempre exibindo o valor da variavel;
+   
+*/
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+
+$num = 9;
+echo "O número escolhido foi ". $num;
+echo "<hr>";
+
+$soma = $num + 5;
+echo "Adicionando 5 o resultado obtido é: ". $soma;
+echo "<hr>";
+
+$sub = $soma - 3;
+echo "Subtraindo 3 o resultado obtido é: ". $sub;
+echo "<hr>";
+
+$mult = $sub * 10;
+echo "Multiplicando por 10 o resultado obtido é: ". $mult;
+echo "<hr>";
+
+$div = $mult / 2;
+echo "Dividindo por 2 o resultado obtido é: ". $div;
+echo "<hr>";
+
+
+
+/*
+ Crie uma variavel cont = 0, utilize o operador de incremento até o valor da variavel ser 10 sempre exibindo o valor da variavel;
     Crie uma variavel cont = 100, utilize o operador de decremento até o valor da variavel ser 89 sempre exibindo o valor da variavel;
 
+*/
+    echo "<br>";
+echo "<br>";
+echo "<hr>";
+
+$cont = 0;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo ++$cont;
+ echo "<hr>";
+
+ $cont = 100;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo --$cont;
+ echo "<hr>"
+
+/*
 Operadores relacionais
 
     Crie duas variáveis num1 e num2, atribua valores quaisquer a elas;
@@ -31,7 +123,17 @@ Operadores relacionais
     Utilize os operadores relacionais e verifique se num1 é menor que num2, utilize a função var_dump() para exibir o resultado;
     Utilize os operadores relacionais e verifique se num1 é maior ou igual que num2, utilize a função var_dump() para exibir o resultado;
     Utilize os operadores relacionais e verifique se num1 é menor ou igual que num2, utilize a função var_dump() para exibir o resultado;
+*/
 
+echo "<br>";
+echo "<br>";
+echo "<hr>";
+
+$num1 = ;
+$num2 = :
+
+
+/*
 Operadores lógicos
 
     Crie duas variáveis booleanas, a e b;
